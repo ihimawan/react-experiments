50d7e7c401d8a408bf4b33021235a6f18786ead6
Indri Himawan <indri.himawan@walmart.com>
2019-09-23 12:20:26 -0400

    using fragments

--- react-exercise/src/components/Persons/Person/Person.js
+++ react-exercise/src/components/Persons/Person/Person.js
@@ -1,26 +1,35 @@
-import React, { useEffect } from 'react';
-import './Person.css';
-import Radium from 'radium';
+import React, { useEffect, Fragment } from "react";
+import "./Person.css";
+import Aux from "../../../hoc/Aux.js";
+import Radium from "radium";
 
-const person = (props) => {
+const person = props => {
+  useEffect(() => {
+    // will execute for every render  cycle
+    console.log("[person.js] useEffects");
+  }, [props.name]); // if props.name changed
 
-    useEffect(() => {
-        // will execute for every render  cycle
-        console.log('[person.js] useEffects');
-    }, [props.name]); // if props.name changed
-
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
+  const style = {
+    "@media (min-width: 500px)": {
+      width: "450px"
     }
+  };
 
-    return (
-        <div className="Person" style={style}>
-            <p onClick={props.click}>My name is {props.name} and I am {props.age} years old {props.children}</p>
-            <input type="text" onChange={props.nameChangeHandler} value={props.name} />
-        </div>
-    );
+  return (
+    <Fragment>
+      <div className="Person" style={style}>
+        <p onClick={props.click}>
+          My name is {props.name} and I am {props.age} years old{" "}
+          {props.children}
+        </p>
+        <input
+          type="text"
+          onChange={props.nameChangeHandler}
+          value={props.name}
+        />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Radium(person);
